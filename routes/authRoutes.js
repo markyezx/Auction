55ae764289eab1b49f497843f6e2c870a6beb443
markyezx <pisanliwrungsub28@gmail.com>
2025-01-23 15:20:19 +0700

    2st

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
-const verifyRefreshToken = require("../middleware/auth");
 
 // Route สำหรับการลงทะเบียน
 router.post("/register", authController.register);
